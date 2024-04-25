812d89790300fbf3b0caba52dcc5c4650919c642
Alejandro Lüthi <alejandro.luthi@gmail.com>
2024-04-25 22:53:57 +0200

    setup project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { db } from './config/firebase'
+import {getDocs, collection} from 'firebase/firestore'
+import {useEffect, useState} from "react";
 
 function App() {
+  //Example for Firebase firestore
+  const [movieList, setMovieList] = useState([])
+  const moviesCollectionsRef = collection(db, "clients")
+  useEffect(() => {
+    const getClientsList = async () => {
+      //READ THE DATA
+      // SET THE CLIENTS DATA
+      try {
+        const data = await getDocs(moviesCollectionsRef);
+        console.log(data);
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getClientsList()
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA5AiuNw79G9IA8IWBsvpTEras98wHND9I",
+    authDomain: "well-belle.firebaseapp.com",
+    projectId: "well-belle",
+    storageBucket: "well-belle.appspot.com",
+    messagingSenderId: "261625581857",
+    appId: "1:261625581857:web:48689ac65874c1f6de2e60",
+    measurementId: "G-5MQT664MLC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
